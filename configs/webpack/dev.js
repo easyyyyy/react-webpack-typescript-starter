4688b579cbd10c56deab263cb6345c89a6a91da6
huangyixi <526282887@qq.com>
2023-01-09 01:29:58 +0800

    feat: mpa

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -2,13 +2,32 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./common");
 const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = merge(commonConfig, {
   mode: "development",
+  entry: {
+    'home': path.resolve(__dirname, '../../src/pages/app/index.tsx'),
+    'cart': path.resolve(__dirname, '../../src/pages/cart/index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'js/[name].[hash]-bundle.js'
+  },
   devServer: {
     hot: true, // enable HMR on the server
     historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
   },
   devtool: "cheap-module-source-map",
-  plugins: [new ReactRefreshPlugin()],
+  plugins: [new ReactRefreshPlugin(),
+    new HtmlWebpackPlugin({
+      chunks: ['home'],
+      filename: 'home.html'
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['cart'],
+      filename: 'cart.html'
+    }),
+  ],
 });
--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,19 +1,30 @@
 // production config
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
   mode: "production",
+  entry: {
+    'home': path.resolve(__dirname, '../../src/pages/app/index.tsx'),
+    'cart': path.resolve(__dirname, '../../src/pages/cart/index.tsx'),
+  },
   output: {
-    filename: "js/bundle.[contenthash].min.js",
-    path: resolve(__dirname, "../../dist"),
-    publicPath: "/",
+    path: path.resolve(__dirname, './build'),
+    filename: 'js/[name].[hash]-bundle.js'
   },
   devtool: "source-map",
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM",
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['home'],
+      filename: 'home.html'
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['cart'],
+      filename: 'cart.html'
+    }),
+  ],
 });
